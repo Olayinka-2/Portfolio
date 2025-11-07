aa8d9708936f3f081251f7ee93406a41e6c2c29d
Olayinka-2 <olayinkaisrael802@gmail.com>
2025-11-07 02:23:32 +0100

    Update the Project Page

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,69 +1,81 @@
-import {Link} from "react-scroll";
-import {Github, Facebook, Linkedin, Twitter} from "lucide-react";
+import { Link } from "react-scroll";
+import { Github, Facebook, Linkedin, Twitter } from "lucide-react";
 
 const Aside = () => {
   return (
     <>
       <div className="w-full lg:w-2/5 lg:sticky top-0 lg:h-screen overflow-y-auto px-6 lg:px-12 flex flex-col bg-slate-950 py-8 md:py-20">
         <div className=" lg:hidden w-32 h-32 md:w-40 md:h-40 rounded-full border-4 border-emerald-400 shadow-lg mb-6 overflow-hidden">
-
-      <img
-  src="/profile.JPG"
-  alt="Israel Olayinka"
-  className="object-cover w-full h-auto object-center"
-/>
+          <img
+            src="/profile.JPG"
+            alt="Israel Olayinka"
+            className="object-cover w-full h-auto object-center"
+          />
         </div>
 
-              <header className="px-3  mb-5">
-                <h1 className="font-bold md:text-5xl text-3xl text-slate-200 ">Israel Olayinka</h1>
-                <p className=" text-md md:text-xl font-bold text-slate-200 mt-2">
-                  FrontEnd Developer
-                </p>
-                <p className="mt-4 text-slate-400">
-                  I build accessible, pixel-perfect digital experiences for the business owners.
-                </p>
+        <header className="px-3  mb-5">
+          <h1 className="font-bold md:text-5xl text-3xl text-slate-200 ">
+            Israel Olayinka
+          </h1>
+          <p className=" text-md md:text-xl font-bold text-slate-200 mt-2">
+            FrontEnd Developer
+          </p>
+          <p className="mt-4 text-slate-400">
+            I build accessible, pixel-perfect digital experiences for the
+            business owners.
+          </p>
 
-                <nav className="mt-10 hidden lg:flex flex-col space-y-4">
-
-                  {["About", "Experience", "Projects", "Contact"].map((item) => (
-                    <Link
-                    key={item}
-                    to={item}
-                    smooth={true}
-                    duration={500}
-                    spy={true}
-                    activeClass="active-link"
-                    className="w-min"
-                  >
-                    <div className="flex items-center gap-2 px-2 text-slate-300 hover:text-slate-100 transition-colors group cursor-pointer">
-                      <span className="line block w-8 h-px bg-slate-300 group-hover:w-16 transition-all"></span>
-                      <span>{item}</span>
-                    </div>
-                  </Link>
-
-                  ))}
-                </nav>
-              </header>
-
-              <footer className="mt-auto px-3">
-                <div className="flex space-x-4 text-lg text-slate-300">
-                  <a href="https://github.com/Olayinka-2" target="_blank" rel="noopener noreferrer">
-                    <Github className="text-slate-400 hover:text-emerald-400 transition cursor-pointer"/>
-                  </a>
-                  <a href="https://facebook.com/olayinka.israel.330/" target="_blank" rel="noopener noreferrer">
-                    <Facebook className="text-slate-400 hover:text-emerald-400 transition cursor-pointer"/>
-                  </a>
-                  <a href="https://www.linkedin.com/in/israel-olayinka-775305307/" target="_blank" rel="noopener noreferrer">
-                    <Linkedin className="text-slate-400 hover:text-emerald-400 transition cursor-pointer"/>
-                  </a>
-                  <a href="#" target="_blank" rel="noopener noreferrer">
-                    <Twitter className="text-slate-400 hover:text-emerald-400 transition cursor-pointer"/>
-                  </a>
+          <nav className="mt-10 hidden lg:flex flex-col space-y-4">
+            {["About", "Experience", "Projects", "Contact"].map((item) => (
+              <Link
+                key={item}
+                to={item}
+                smooth={true}
+                duration={500}
+                spy={true}
+                activeClass="active-link"
+                className="w-min"
+              >
+                <div className="flex items-center gap-2 px-2 text-slate-300 hover:text-slate-100 transition-colors group cursor-pointer">
+                  <span className="line block w-8 h-px bg-slate-300 group-hover:w-16 transition-all"></span>
+                  <span>{item}</span>
                 </div>
-              </footer>
-            </div>
+              </Link>
+            ))}
+          </nav>
+        </header>
+
+        <footer className="mt-auto px-3">
+          <div className="flex space-x-4 text-lg text-slate-300">
+            <a
+              href="https://github.com/Olayinka-2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Github className="text-slate-400 hover:text-emerald-400 transition cursor-pointer" />
+            </a>
+            <a
+              href="https://facebook.com/olayinka.israel.330/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Facebook className="text-slate-400 hover:text-emerald-400 transition cursor-pointer" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/israel-olayinka-775305307/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Linkedin className="text-slate-400 hover:text-emerald-400 transition cursor-pointer" />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <Twitter className="text-slate-400 hover:text-emerald-400 transition cursor-pointer" />
+            </a>
+          </div>
+        </footer>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Aside;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { ArrowLeftIcon, ArrowUpRight } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import projects from "../components/data/ProjectData";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="bg-slate-950 min-h-screen">
+      <div className="max-w-screen-xl mx-auto text-white py-5 w-[95%] sm:w-[90%]">
+        <div className="px-6 lg:px-10 py-8 md:py-17">
+          <Link to={"/"}>
+            <p className="flex items-center font-bold text-emerald-400 gap-2 cursor-pointer">
+              <ArrowLeftIcon />
+              Israel Olayinka
+            </p>
+          </Link>
+          <h1 className="text-2xl md:text-4xl font-extrabold">My Projects</h1>
+          <div className="mt-10">
+            <div className="sticky top-0 bg-slate-950/80 backdrop-blur-md border-b border-slate-800">
+              <div className="grid grid-cols-[2fr_3fr] sm:grid-cols-[1fr_3fr_3fr] lg:grid-cols-[1fr_3fr_5fr_3fr] sm:py-3 py-5 text-lg">
+                <div className="font-semibold text-gray-200">Year</div>
+                <div className="font-semibold text-gray-200">Project</div>
+                <div className="font-semibold text-gray-200 hidden lg:block">
+                  Built With
+                </div>
+                <div className="font-semibold text-gray-200 hidden sm:block">
+                  Link
+                </div>
+              </div>
+            </div>
+            {projects.map((project, idx) => (
+              <div
+                className="grid grid-cols-[2fr_3fr] sm:grid-cols-[1fr_3fr_3fr] lg:grid-cols-[1fr_3fr_5fr_3fr] items-start  sm:py-5 py-4 border-b border-slate-800"
+                key={idx}
+              >
+                <div className="text-gray-500">{project.year}</div>
+                <div>
+                  <div className="hidden sm:block">{project.title}</div>
+                  <div>
+                    <a
+                      href={project.liveDemo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-emerald-500 text-base font-semibold  text-white block sm:hidden"
+                    >
+                      {project.title}
+                      <ArrowUpRight className="w-3 inline-block -mt-1.5" />
+                    </a>
+                  </div>
+                </div>
+                <div className=" flex-wrap gap-2 mt-4 hidden lg:flex">
+                  {project.technologies.map((tech, idx) => (
+                    <span
+                      key={idx}
+                      className="px-3 py-1 text-xs font-medium rounded-full text-emerald-400 bg-emerald-400/10"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+                <div className="hidden sm:block">
+                  <a
+                    href={project.liveDemo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-emerald-500 text-base font-semibold  text-gray-500"
+                  >
+                    {new URL(project.liveDemo).hostname.replace(/^www\./, "")}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
